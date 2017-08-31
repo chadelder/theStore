876d91fddb4a3e3d7176f9b60d34b196cf757a30
chadelder <c.j.elder@outlook.com>
2017-08-31 18:57:27 -0400

    83117

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //'secret': 'jerome is the secret',
+  'secret': 'jerome is the secret',
   //'database': 'mongodb://localhost/tokenAPI'
   'database': 'mongodb://chud:jerome@ds161443.mlab.com:61443/lombardymarket'
 };
