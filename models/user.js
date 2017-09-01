0032090044a254ce32e5c6d6282e4650dc070553
chadelder <c.j.elder@outlook.com>
2017-08-31 21:42:42 -0400

    83117

--- models/user.js
+++ models/user.js
@@ -30,7 +30,7 @@ UserSchema.pre("save", function (next) {
     var user = this;
     if (!user.isModified("password")) return next();
 
-    bcrypt.hash(user.password, salt, null, function (err, hash) {
+    bcrypt.hash(user.password, null, null, function (err, hash) {
         if (err) return next(err);
 
         user.password = hash;
